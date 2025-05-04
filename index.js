994c69230184d0a80367892edb4dac8fd395b561
arkhannanov <arkhannanov@gail.com>
2025-05-04 09:29:51 +0300

    feat: add validation

--- index.js
+++ index.js
@@ -11,56 +11,74 @@ const groupChatId = '-1002505161849';
 const userStates = {};
 
 bot.command('start', async (ctx) => {
-    const keyboard = new InlineKeyboard()
-        .text('Подключиться бесплатно', 'freeConnect')
-        .row()
-        .url('Перейти на сайт', 'http://cippa.ru/');
+    try {
+        const keyboard = new InlineKeyboard()
+            .text('Подключиться бесплатно', 'freeConnect')
+            .row()
+            .url('Перейти на сайт', 'http://cippa.ru/');
 
-    await ctx.replyWithPhoto(new InputFile(filePath), {
-        caption: 'Привет! Данный бот поможет подключиться \n' +
-            'к системе ЦИППА, это позволит пользоваться привилегиями персональной защиты прямо сейчас.',
-        reply_markup: keyboard,
-    });
+        await ctx.replyWithPhoto(new InputFile(filePath), {
+            caption: 'Привет! Данный бот поможет подключиться \n' +
+                'к системе ЦИППА, это позволит пользоваться привилегиями персональной защиты прямо сейчас.',
+            reply_markup: keyboard,
+        });
+    } catch (error) {
+        console.error('Ошибка в команде start:', error);
+    }
 });
 
 bot.callbackQuery('freeConnect', async (ctx) => {
-    await ctx.answerCallbackQuery({ text: 'Процесс подключения начался!' });
-    await ctx.reply('Давайте начнем процесс подключения.');
+    try {
+        await ctx.answerCallbackQuery({ text: 'Процесс подключения начался!' });
+        await ctx.reply('Давайте начнем процесс подключения.');
 
-    userStates[ctx.from.id] = { step: 1 };
-    await ctx.reply('Как Вас зовут?');
+        userStates[ctx.from.id] = { step: 1 };
+        await ctx.reply('Как Вас зовут?');
+    } catch (error) {
+        console.error('Ошибка в обработке колбэка freeConnect:', error);
+    }
 });
 
 bot.on('message', async (ctx) => {
-    const state = userStates[ctx.from.id];
+    try {
+        const state = userStates[ctx.from.id];
 
-    if (!state) {
-        return; // Если состояние не найдено, игнорируем
-    }
+        if (!state) {
+            return; // Если состояние не найдено, игнорируем
+        }
 
-    switch (state.step) {
-        case 1:
-            state.name = ctx.message.text;
-            state.step = 2;
-            await ctx.reply('Номер телефона:');
-            break;
-        case 2:
-            state.phone = ctx.message.text;
-            state.step = 3;
-            await ctx.reply('Код Агента:');
-            break;
-        case 3:
-            state.agentCode = ctx.message.text;
+        switch (state.step) {
+            case 1:
+                state.name = ctx.message.text;
+                state.step = 2;
+                await ctx.reply('Номер телефона:');
+                break;
+            case 2:
+                const phoneRegex = /^[+]?[0-9]{10,15}$/;  // Простая регулярка для номера телефона
+                if (phoneRegex.test(ctx.message.text)) {
+                    state.phone = ctx.message.text;
+                    state.step = 3;
+                    await ctx.reply('Код Агента:');
+                } else {
+                    await ctx.reply('Введите правильный номер телефона, пожалуйста. Пример: +7234567890 или 8234567890');
+                }
+                break;
 
-            // Отправка собранных данных в группу
-            await bot.api.sendMessage(groupChatId, `Новый пользователь:\nИмя: ${state.name}\nТелефон: ${state.phone}\nКод Агента: ${state.agentCode}`);
+            case 3:
+                state.agentCode = ctx.message.text;
 
-            // Ответ пользователю
-            await ctx.reply('Спасибо за предоставленную информацию!');
+                // Отправка собранных данных в группу
+                await bot.api.sendMessage(groupChatId, `Новый пользователь:\nИмя: ${state.name}\nТелефон: ${state.phone}\nКод Агента: ${state.agentCode}`);
 
-            // Удаляем состояние пользователя
-            delete userStates[ctx.from.id];
-            break;
+                // Ответ пользователю
+                await ctx.reply('Спасибо за предоставленную информацию!');
+
+                // Удаляем состояние пользователя
+                delete userStates[ctx.from.id];
+                break;
+        }
+    } catch (error) {
+        console.error('Ошибка в обработке сообщения:', error);
     }
 });
 
